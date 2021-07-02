1d36787cd982d307a95fd03117382fbd914e71e5
Sefa Uzunoğlu <sefauzunogluu@gmail.com>
2021-07-02 17:36:06 +0300

    cesitli guzellikler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Model } from './model';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,16 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'SocialApp';
-  categoryName = "Telefon";
 
-  products = [
-    { id: 1, name: "Samsung S5", price: 2000, isActive: true },
-    { id: 2, name: "Samsung S6", price: 3000, isActive: false },
-    { id: 3, name: "Samsung S7", price: 4000, isActive: true },
-    { id: 4, name: "Samsung S8", price: 5000, isActive: false },
-    { id: 5, name: "Samsung S9", price: 6000, isActive: true },
-  ];
+  model = new Model();
+
+  // model bilgisi içerisindeki bilgilere ulaşmak için ise;
+
+  getCatName() {
+    return this.model.categoryName;
+  }
+
+  getProducts() {
+    return this.model.products;
+  }
 }
